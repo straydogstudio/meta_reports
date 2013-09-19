9acb84d05f3b687aa097f8dfb412887cfaa6d135
Noel Peden <noel@peden.biz>
2013-09-18 23:47:19 -0700

    Test pdf / xlsx generation

--- spec/features/dummy_spec.rb
+++ spec/features/dummy_spec.rb
@@ -21,4 +21,22 @@ describe 'MetaReports dummy app' do
     visit "/meta_reports/#{@report_moo.id}"
     page.should have_content "Ode to Moo"
   end
+
+  it "downloads report using pdf" do
+    visit "/meta_reports/#{@report_moo.id}.pdf"
+    response_headers["Content-Type"].should == "application/pdf; charset=utf-8"
+    output = PDF::Inspector::Text.analyze(page.source)
+    output.strings.should == ["Company Name", "1234 Address", "City, ST 12345", "(123) 456-7890", "Le Moo", "Ahem", "The Big Moo", "Number", "Title", "Hey", "1", "Ode to Moo", "Ow", "2", "Odious Moo", "Eww", "Page 1 of 1"]
+  end
+
+  it "downloads report using xlsx" do
+    visit "/meta_reports/#{@report_moo.id}.xlsx"
+    page.response_headers['Content-Type'].should == Mime::XLSX.to_s + "; charset=utf-8"
+    File.open('/tmp/meta_reports.xlsx', 'w') {|f| f.write(page.source) }
+    wb = nil
+    expect{ wb = Excelx.new('/tmp/meta_reports.xlsx') }.to_not raise_error
+    wb.cell(3,1).should == 'Le Moo'
+    wb.cell(6,1).should == 'The Big Moo'
+    wb.cell(9,2).should == 'Odious Moo'
+  end
 end
