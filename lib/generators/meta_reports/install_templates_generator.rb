5aefcca2ebcb6eac9395ab486e31acb36787e5b8
Noel Peden <noel@peden.biz>
2013-10-07 23:34:08 -0700

    Use non activerecord report model

--- lib/generators/meta_reports/install_templates_generator.rb
+++ lib/generators/meta_reports/install_templates_generator.rb
@@ -9,7 +9,7 @@ module MetaReports
 
       def install_models
         directory "app/models/meta_reports", "app/models/meta_reports"
-        copy_file "lib/generators/meta_reports/templates/models/report.rb", "app/models/meta_reports/report.rb"
+        copy_file "lib/generators/meta_reports/templates/models/report_non_activerecord.rb", "app/models/meta_reports/report.rb"
       end
 
       def install_helper
@@ -17,7 +17,7 @@ module MetaReports
       end
 
       def install_views
-        directory "lib/generators/meta_reports/templates/views/templates", "app/views/meta_reports"
+        directory "lib/generators/meta_reports/templates/views/templates", "app/views/meta_reports/reports/templates"
       end
     end
   end
---
+++ lib/generators/meta_reports/templates/models/report_non_activerecord.rb
@@ -0,0 +1,30 @@
+class MetaReports::Report
+
+  #
+  # Shared colors. The key is the class name, value is RGB in hex format
+  #
+
+  COLORS = {
+    even:                   'efefef',
+    odd:                    'ffffff',
+  }
+
+
+  #
+  # Reports
+  #
+
+  def self.example_report(params)
+    {title: 'Example Report', subtitle: 'this is a test report', tables: {'Table 1' => [['One','Two','Three'],[1,2,3]]}}
+    MetaReports::Data.new do |d|
+      d.title = 'Example Report'
+      d.subtitle = 'Of the Testing Kind'
+      d.description = 'This is a test report.'
+      d.tables["Table 1"] = MetaReports::Table.new do |t|
+        t << ['One', 'Two', 'Three']
+        t << [1, 2, 3]
+      end
+    end
+  end
+
+end
