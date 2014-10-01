4ddfbeec5a9a87ad89384a8a2cc9e474048276f7
Noel Peden <noel@peden.biz>
2014-10-01 14:34:13 -0700

    Copy templates for each test

--- spec/ci.sh
+++ spec/ci.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-cd spec/dummy && echo "bundle install" && bundle install --without debug && echo "rake db:create" && bundle exec rake db:create && echo "rake db:migrate" && bundle exec rake db:migrate && cd ../../ && echo "rspec spec" && bundle exec rspec spec
+cd spec/dummy && echo "bundle install" && bundle install --without debug && echo "rake db:create" && bundle exec rake db:create && echo "rake db:migrate" && bundle exec rake db:migrate && rm -rf app/models/meta_reports/color.rb app/views/meta_reports && bundle exec rails g meta_reports:install_templates && cd ../../ && echo "rspec spec" && bundle exec rspec spec
