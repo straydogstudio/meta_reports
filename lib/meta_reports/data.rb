a9ce83d3693c537df2632fc3b42769a8e11adc46
Noel Peden <noel@peden.biz>
2014-10-02 16:30:51 -0700

    Declare module

--- lib/meta_reports/data.rb
+++ lib/meta_reports/data.rb
@@ -1,43 +1,45 @@
-class MetaReports::Data
-  def initialize
-    @hash = {tables: {}}
-    @id = rand(10000)
-    yield self if block_given?
-    self
-  end
-
-  def method_missing(method, *args, &block)
-    method_string = method.to_s
-    if method_string =~ /^(.+)=$/
-      @hash[$1.to_sym] = args.first
-    elsif @hash[method.to_sym]
-      @hash[method.to_sym]
-    else
-      @hash.send(method, *args)
+module MetaReports
+  class Data
+    def initialize
+      @hash = {tables: {}}
+      @id = rand(10000)
+      yield self if block_given?
+      self
     end
-  end
 
-  def [](key)
-    @hash[key]
-  end
+    def method_missing(method, *args, &block)
+      method_string = method.to_s
+      if method_string =~ /^(.+)=$/
+        @hash[$1.to_sym] = args.first
+      elsif @hash[method.to_sym]
+        @hash[method.to_sym]
+      else
+        @hash.send(method, *args)
+      end
+    end
 
-  def []=(key, value)
-    @hash[key] = value
-  end
+    def [](key)
+      @hash[key]
+    end
 
-  def id
-    @hash[:id] || @hash[:title].to_s.downcase.gsub(/[^a-z]/,'_') || @id
-  end
+    def []=(key, value)
+      @hash[key] = value
+    end
 
-  def tables
-    @hash[:tables]
-  end
+    def id
+      @hash[:id] || @hash[:title].to_s.downcase.gsub(/[^a-z]/,'_') || @id
+    end
 
-  def tables=(value)
-    @hash[:tables] = value
-  end
+    def tables
+      @hash[:tables]
+    end
 
-  def to_h
-    @hash
+    def tables=(value)
+      @hash[:tables] = value
+    end
+
+    def to_h
+      @hash
+    end
   end
 end
--- lib/meta_reports/table.rb
+++ lib/meta_reports/table.rb
@@ -1,84 +1,86 @@
-class MetaReports::Table
-  def initialize
-    @data = []
-    @options = {row_classes: {}}
-    yield self if block_given?
-  end
-
-  def method_missing(method, *args, &block)
-    method_string = method.to_s
-    if method_string =~ /^(.+)=$/
-      @options[$1.to_sym] = args.first
-    elsif @options[method.to_sym]
-      @options[method.to_sym]
-    else
-      super
+module MetaReports
+  class Table
+    def initialize
+      @data = []
+      @options = {row_classes: {}}
+      yield self if block_given?
     end
+
+    def method_missing(method, *args, &block)
+      method_string = method.to_s
+      if method_string =~ /^(.+)=$/
+        @options[$1.to_sym] = args.first
+      elsif @options[method.to_sym]
+        @options[method.to_sym]
+      else
+        super
+      end
+    end
+
+    # options methods
+
+    def [](key)
+      @options[key]
+    end
+
+    def []=(key,val)
+      @options[key] = val
+    end
+
+    def options
+      @options
+    end
+
+    def row_classes
+      @options[:row_classes]
+    end
+
+    # data methods
+
+    def <<(val)
+      @data << val
+    end
+
+    def +(arr)
+      @data += arr
+    end
+
+    def first
+      @data.first
+    end
+
+    def last
+      @data.last
+    end
+
+    def length
+      @data.length
+    end
+
+    def pop
+      @data.pop
+    end
+
+    def push(val)
+      @data.push(val)
+    end
+
+    def shift
+      @data.shift
+    end
+
+    def to_a
+      @data
+    end
+
+    def data=(value)
+      @data = value
+    end
+
+    def unshift(val)
+      @data.unshift(val)
+    end
+
+    alias_method :data, :to_a
   end
-
-  # options methods
-
-  def [](key)
-    @options[key]
-  end
-
-  def []=(key,val)
-    @options[key] = val
-  end
-
-  def options
-    @options
-  end
-
-  def row_classes
-    @options[:row_classes]
-  end
-
-  # data methods
-
-  def <<(val)
-    @data << val
-  end
-
-  def +(arr)
-    @data += arr
-  end
-
-  def first
-    @data.first
-  end
-
-  def last
-    @data.last
-  end
-
-  def length
-    @data.length
-  end
-
-  def pop
-    @data.pop
-  end
-
-  def push(val)
-    @data.push(val)
-  end
-
-  def shift
-    @data.shift
-  end
-
-  def to_a
-    @data
-  end
-
-  def data=(value)
-    @data = value
-  end
-
-  def unshift(val)
-    @data.unshift(val)
-  end
-
-  alias_method :data, :to_a
 end
